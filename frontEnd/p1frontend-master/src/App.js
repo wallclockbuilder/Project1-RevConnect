37ee014adb59ebd8596b3e89f015335253808720
Mawuli Adzoe <mawuli738@revature.net>
2024-07-30 09:27:42 -0700

    Rebase  /follow page and follow component. Work in Progress

--- frontEnd/p1frontend-master/src/App.js
+++ frontEnd/p1frontend-master/src/App.js
@@ -9,12 +9,12 @@ import MyPosts from "./pages/MyPosts";
 import AppNavbar from "./components/Navbar";
 import ChatPage from "./pages/ChatPage";
 import ChatRoom from "./components/ChatRoom";
-<<<<<<< HEAD
+import MyPosts from "./pages/MyPosts";
 import Follow from "./components/Follow";
 
 
-const App = () => {
-=======
+
+function App() {
 const App: React.FC = () => {
 >>>>>>> 10441f2 ( added back the chatpage and chatroom in the app.js , added search button and input in the navbar)
     const location = useLocation();
